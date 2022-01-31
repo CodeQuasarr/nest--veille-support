c2ba7f5b638ac532b891e1552ee535a3f92967dc
ANYARONKE DARE SAMUEL <dsanyaronke@gmail.com>
2022-01-31 07:56:13 +0100

    TEST

--- App/Views/layout/default.php
+++ App/Views/layout/default.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
-    <link rel="stylesheet" href="public/assets/css/main.css">
+    <link rel="stylesheet" href="../../../public/assets/css/main.css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -17,10 +17,11 @@
 <body>
     <div class="container-fluid p-0 m-0 d-flex align-items-stresh">
         <?= $contents ?>
-        <footer style="height: 100px;font-size:11px" class="bg-dark py-5 text-white d-flex align-items-center justify-content-center">
-            <span>© 2021 Daré Samuel ANYARONKE, Tous droits réservés.</span>
-        </footer>
+        
     </div>
+    <footer style="height: 100px;font-size:11px" class="bg-dark py-5 text-white d-flex align-items-center justify-content-center">
+            <span>© 2021 Daré Samuel ANYARONKE, Tous droits réservés.</span>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js "></script>
 </body>
 
