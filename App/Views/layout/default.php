f637645e878ea5d5967f430c1b5d4db5e9ac2b46
ANYARONKE DARE SAMUEL <dsanyaronke@gmail.com>
2022-02-01 00:04:52 +0100

    V2

--- App/Views/layout/default.php
+++ App/Views/layout/default.php
@@ -106,13 +106,48 @@
 </head>
 
 <body>
-    <div class="container-fluid p-0 m-0 d-flex align-items-stresh">
+<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+        <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+            <path
+                d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+        </symbol>
+    </svg>
+    <div class="container-fluid d-flex flew-wrap align-items-stresh p-0 m-0 position relative">
+
+        <div style="background-color:#12141C" class="left col-md-3 position-fixed d-none d-md-block">
+            <div class="py-4 col-12 d-flex align-items-center justify-content-center">
+                <img height="50" width="50" src="public/web/logo/nest.svg" alt="">
+                <h1 class="ms-4 next-text-color"> <a style="text-decoration: none;" class="next-text-color"
+                        target="_blank" href="https://NestJS.com/">NestJS</a></h1>
+            </div>
+            <nav class="nav flex-column pt-5">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <h2><a class="nav-link active" aria-current="page" href="#about">Quoi de neuf</a>
+                        </h2>
+                    </li>
+                    <li class="nav-item">
+                        <h2><a class="nav-link" href="#install">Installation</a></h2>
+
+                    </li>
+                    <li class="nav-item">
+                        <h2> <a class="nav-link" href="#base">Composant de base</a></h2>
+
+                    </li>
+                    <li class="nav-item">
+                        <h2><a class="nav-link" href="#tuto">Tuto</a></h2>
+
+                    </li>
+                    <li class="nav-item">
+                        <h2><a class="nav-link" href="#conclure">Fin</a></h2>
+
+                    </li>
+                </ul>
+            </nav>
+        </div>
         <?= $contents ?>
 
     </div>
-    <footer class="bg-dark text-white d-flex align-items-center justify-content-center">
-        <span>© 2021 Daré Samuel ANYARONKE, Tous droits réservés.</span>
-    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js "></script>
 </body>
 
